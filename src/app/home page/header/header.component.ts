40f73e4f3ede70bff6fa136bec8ce774b2f0db13
NevenaJuric <nevenajuric96@gmail.com>
2020-07-17 10:30:18 +0200

    add partica js

--- src/app/home page/header/header.component.ts
+++ src/app/home page/header/header.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var particlesJS: any;
+
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // tslint:disable-next-line: only-arrow-functions
+    particlesJS.load('particles-js', 'assets/data/particlesjs-config.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
   }
-
 }
--- src/app/home page/main/main.component.ts
+++ src/app/home page/main/main.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
 })
 export class MainComponent implements OnInit {
+
+  faLaptopCode = faLaptopCode;
   constructor() {}
 
   ngOnInit() {}

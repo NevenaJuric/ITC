8cc31169685fd8e7c95ee2ec19a7e97dd6e48712
NevenaJuric <nevenajuric96@gmail.com>
2020-07-21 16:14:37 +0200

    add scroll event

--- src/app/home page/main/main.component.ts
+++ src/app/home page/main/main.component.ts
@@ -8,7 +8,19 @@ import {faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 export class MainComponent implements OnInit {
 
   faLaptopCode = faLaptopCode;
+  scroll = false;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    window.addEventListener('scroll', this.scrolling, true);
+  }
+
+  scrolling = (s) => {
+    const sc = s.target.scrollingElement.scrollTop;
+    if (sc > window.innerHeight){
+      this.scroll = true;
+    }else{
+      this.scroll = false;
+    }
+  }
 }

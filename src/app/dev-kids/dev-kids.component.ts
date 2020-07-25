2b92b0faf622c259327c6b03667c6a4b998cb529
NevenaJuric <nevenajuric96@gmail.com>
2020-07-25 19:56:02 +0200

    add icons

--- src/app/dev-kids/dev-kids.component.ts
+++ src/app/dev-kids/dev-kids.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { faRobot } from '@fortawesome/free-solid-svg-icons';
+import { faGamepad } from '@fortawesome/free-solid-svg-icons';
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+import { faPalette } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dev-kids',
   templateUrl: './dev-kids.component.html'
 })
 export class DevKidsComponent implements OnInit {
+
+  faRobot = faRobot;
+  faGamepad = faGamepad;
+  faLaptopCode = faLaptopCode;
+  faPalette = faPalette;
 
   constructor() { }
 
